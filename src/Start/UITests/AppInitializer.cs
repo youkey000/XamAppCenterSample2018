22cb2659b32b708fd6bc797a5491c6991b7c4052
草野祐季(Dev) <yuki-kusano-dev@MyMacBookPro.local>
2019-02-23 15:44:34 +0900

    test

--- src/Start/UITests/AppInitializer.cs
+++ src/Start/UITests/AppInitializer.cs
@@ -1,1 +1,35 @@
-﻿ 
+﻿using Xamarin.UITest;
+
+namespace XamAppCenterSample2018.UITests
+{
+    public class AppInitializer
+    {
+    }
+}
+
+namespace XamAppCenterSample2018.UITests
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    .EnableLocalScreenshots()
+                    .PreferIdeSettings()
+                    .InstalledApp("<あなたのアプリのパッケージ名>")
+                    .StartApp();
+            }
+
+            return ConfigureApp
+                .iOS
+                .EnableLocalScreenshots()
+                .PreferIdeSettings()
+                //info.plistのバンドル識別子
+                .InstalledApp("com.yuki_123_0987.XamAppCenterSample2018")
+                .StartApp();
+        }
+    }
+}
--- src/StartShort/XamAppCenterSample2018/Variables.cs
+++ src/StartShort/XamAppCenterSample2018/Variables.cs
@@ -5,6 +5,6 @@ namespace XamAppCenterSample2018
     public static class Variables
     {
         // NOTE: Replace this example key with a valid subscription key.
-        public static readonly string ApiKey = "<ここに置換え用の文字列を入れる>";
+        public static readonly string ApiKey = "2dd737c864a54c3a9c91d336478eb0ee";
     }
 }
